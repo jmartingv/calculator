adcb8798234654ea735e159138c61f2c60a9e60e
Jesus <jmartingv96@gmail.com>
2022-06-13 21:49:54 -0700

    added basic funcitions

---
+++ script.js
@@ -0,0 +1,29 @@
+/**
+ * Calculator functions
+ *  - addition
+ *  - substraction
+ *  - divison
+ *  - multiplication
+ *  - operate function
+ */
+
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const divide = (a, b) => a / b;
+const multiply = (a, b) => a * b;
+
+const operate = (a, b, operator) => {
+  switch (operator) {
+    case '+':
+      return add(a, b);
+      break;
+    case '-':
+      return substract(a, b);
+      break;
+    case '/':
+      return divide(a, b);
+      break;
+    case '*':
+      return multiply(a, b);
+  }
+};
